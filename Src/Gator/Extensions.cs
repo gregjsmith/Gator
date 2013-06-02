4a7e06cd694529bde21b507216bc33a6c7af1146
Greg Smith <gregjsmith@live.co.uk>
2013-06-02 17:35:46 +0100

    Replaced ss text with json net for indenting support

--- Src/Gator/Extensions.cs
+++ Src/Gator/Extensions.cs
@@ -1,5 +1,4 @@
 using System;
-using ServiceStack.Text;
 
 namespace Gator
 {
@@ -12,7 +11,7 @@ namespace Gator
                 return new Initialize();
             }
 
-            throw new InvalidOperationException("Arguments -- " + args.Join(" ") + " are not valid");
+            throw new InvalidOperationException("Arguments -- " + string.Join(" ", args) + " are not valid");
         }
     }
 }
--- Src/Gator/Initialize.cs
+++ Src/Gator/Initialize.cs
@@ -1,5 +1,6 @@
 using System.IO;
-using ServiceStack.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Gator
 {
@@ -9,7 +10,8 @@ namespace Gator
         {
             var cfg = new DbConfig { type = "unspecified", connectionString = "", currentVersion = "none" };
 
-            File.AppendAllText(App.WorkingDirectory + "database.json", JsonSerializer.SerializeToString(cfg));
+            File.AppendAllText(App.WorkingDirectory + "database.json",
+                               JsonConvert.SerializeObject(cfg, Formatting.Indented, new IsoDateTimeConverter()));
 
             Directory.CreateDirectory(App.WorkingDirectory + "versions");
         }
